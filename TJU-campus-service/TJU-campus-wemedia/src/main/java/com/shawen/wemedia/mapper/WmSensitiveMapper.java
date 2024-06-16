7414aee159fe75bff285098826ccdd93c7f53640
shawenher <2842368912@qq.com>
2024-06-17 01:03:41 +0800

    part-04 实现使用 DFA 自定义敏感词过滤！

---
+++ TJU-campus-service/TJU-campus-wemedia/src/main/java/com/shawen/wemedia/mapper/WmSensitiveMapper.java
@@ -0,0 +1,9 @@
+package com.shawen.wemedia.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shawen.model.wemedia.pojos.WmSensitive;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface WmSensitiveMapper extends BaseMapper<WmSensitive> {
+}
--- TJU-campus-service/TJU-campus-wemedia/src/main/java/com/shawen/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
+++ TJU-campus-service/TJU-campus-wemedia/src/main/java/com/shawen/wemedia/service/impl/WmNewsAutoScanServiceImpl.java
@@ -1,17 +1,23 @@
 package com.shawen.wemedia.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.shawen.apis.article.IArticleClient;
 import com.shawen.file.service.FileStorageService;
 import com.shawen.model.article.dtos.ArticleDto;
 import com.shawen.model.common.dtos.ResponseResult;
 import com.shawen.model.wemedia.pojos.WmChannel;
 import com.shawen.model.wemedia.pojos.WmNews;
+import com.shawen.model.wemedia.pojos.WmSensitive;
 import com.shawen.model.wemedia.pojos.WmUser;
+import com.shawen.utils.common.SensitiveWordUtil;
 import com.shawen.wemedia.mapper.WmChannelMapper;
 import com.shawen.wemedia.mapper.WmNewsMapper;
+import com.shawen.wemedia.mapper.WmSensitiveMapper;
 import com.shawen.wemedia.mapper.WmUserMapper;
 import com.shawen.wemedia.service.WmNewsAutoScanService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -19,6 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -43,7 +50,11 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
 
         if(wmNews.getStatus().equals(WmNews.Status.SUBMIT.getCode())){
             //从内容中提取纯文本内容和图片        暂时不实现审核
-//            Map<String,Object> textAndImages = handleTextAndImages(wmNews);
+            Map<String,Object> textAndImages = handleTextAndImages(wmNews);
+
+            //自管理的敏感词过滤
+            boolean isSensitive = handleSensitiveScan((String) textAndImages.get("content"), wmNews);
+            if(!isSensitive) return;
 
             //2.审核文本内容  阿里云接口  暂时不实现审核
 //            boolean isTextScan = handleTextScan((String) textAndImages.get("content"),wmNews);
@@ -124,7 +135,81 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
         wmNews.setReason(reason);
         wmNewsMapper.updateById(wmNews);
     }
+
+
+    @Autowired
+    private WmSensitiveMapper wmSensitiveMapper;
+
+    /**
+     * 自管理的敏感词审核
+     * @param content
+     * @param wmNews
+     * @return
+     */
+    private boolean handleSensitiveScan(String content, WmNews wmNews) {
+
+        boolean flag = true;
+
+        //获取所有的敏感词
+        List<WmSensitive> wmSensitives = wmSensitiveMapper.selectList(Wrappers.<WmSensitive>lambdaQuery().select(WmSensitive::getSensitives));
+        List<String> sensitiveList = wmSensitives.stream().map(WmSensitive::getSensitives).collect(Collectors.toList());
+
+        //初始化敏感词库
+        SensitiveWordUtil.initMap(sensitiveList);
+
+        //查看文章中是否包含敏感词
+        Map<String, Integer> map = SensitiveWordUtil.matchWords(content);
+        if(map.size() >0){
+            updateWmNews(wmNews,(short) 2,"当前文章中存在违规内容"+map);
+            flag = false;
+        }
+
+        return flag;
+    }
+
+
+    /**
+     * 1。从自媒体文章的内容中提取文本和图片
+     * 2.提取文章的封面图片
+     * @param wmNews
+     * @return
+     */
+
+
+    private Map<String, Object> handleTextAndImages(WmNews wmNews) {
+
+        //存储纯文本内容
+        StringBuilder stringBuilder = new StringBuilder();
+
+        List<String> images = new ArrayList<>();
+
+        //1。从自媒体文章的内容中提取文本和图片
+        if(StringUtils.isNotBlank(wmNews.getContent())){
+            List<Map> maps = JSONArray.parseArray(wmNews.getContent(), Map.class);
+            for (Map map : maps) {
+                if (map.get("type").equals("text")){
+                    stringBuilder.append(map.get("value"));
+                }
+
+                if (map.get("type").equals("image")){
+                    images.add((String) map.get("value"));
+                }
+            }
+        }
+        //2.提取文章的封面图片
+        if(StringUtils.isNotBlank(wmNews.getImages())){
+            String[] split = wmNews.getImages().split(",");
+            images.addAll(Arrays.asList(split));
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("content",stringBuilder.toString());
+        resultMap.put("images",images);
+        return resultMap;
+
+    }
 }
+
 
 
 
@@ -229,45 +314,3 @@ public class WmNewsAutoScanServiceImpl implements WmNewsAutoScanService {
 
 
 
-    /**
-     * 1。从自媒体文章的内容中提取文本和图片
-     * 2.提取文章的封面图片
-     * @param wmNews
-     * @return
-     */
-
-//暂时不做具体的实现
-
-//    private Map<String, Object> handleTextAndImages(WmNews wmNews) {
-//
-//        //存储纯文本内容
-//        StringBuilder stringBuilder = new StringBuilder();
-//
-//        List<String> images = new ArrayList<>();
-//
-//        //1。从自媒体文章的内容中提取文本和图片
-//        if(StringUtils.isNotBlank(wmNews.getContent())){
-//            List<Map> maps = JSONArray.parseArray(wmNews.getContent(), Map.class);
-//            for (Map map : maps) {
-//                if (map.get("type").equals("text")){
-//                    stringBuilder.append(map.get("value"));
-//                }
-//
-//                if (map.get("type").equals("image")){
-//                    images.add((String) map.get("value"));
-//                }
-//            }
-//        }
-//        //2.提取文章的封面图片
-//        if(StringUtils.isNotBlank(wmNews.getImages())){
-//            String[] split = wmNews.getImages().split(",");
-//            images.addAll(Arrays.asList(split));
-//        }
-//
-//        Map<String, Object> resultMap = new HashMap<>();
-//        resultMap.put("content",stringBuilder.toString());
-//        resultMap.put("images",images);
-//        return resultMap;
-//
-//    }
-//}
