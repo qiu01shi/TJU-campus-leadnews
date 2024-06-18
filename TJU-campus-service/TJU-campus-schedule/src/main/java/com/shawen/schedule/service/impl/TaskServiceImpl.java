1dfaf600f1e91c8006d9195f812879433255d363
shawenher <2842368912@qq.com>
2024-06-18 14:22:32 +0800

    part-05 使用 redis 分布式锁，解决多台服务刷新的问题。

--- TJU-campus-service/TJU-campus-schedule/src/main/java/com/shawen/schedule/service/impl/TaskServiceImpl.java
+++ TJU-campus-service/TJU-campus-schedule/src/main/java/com/shawen/schedule/service/impl/TaskServiceImpl.java
@@ -209,20 +209,25 @@ public class TaskServiceImpl implements TaskService {
      */
     @Scheduled(cron = "0 */1 * * * ?")
     public void refresh() {
-        System.out.println(System.currentTimeMillis() / 1000 + "执行了定时任务");
 
-        // 获取所有未来数据集合的key值
-        Set<String> futureKeys = cacheService.scan(ScheduleConstants.FUTURE + "*");// future_*
-        for (String futureKey : futureKeys) { // future_250_250
+        String token = cacheService.tryLock("FUTURE_TASK_SYNC", 1000 * 30);
 
-            // 获取当前数据的  key  topic
-            String topicKey = ScheduleConstants.TOPIC + futureKey.split(ScheduleConstants.FUTURE)[1];
-            //获取该组key下当前需要消费的任务数据
-            Set<String> tasks = cacheService.zRangeByScore(futureKey, 0, System.currentTimeMillis());
-            if (!tasks.isEmpty()) {
-                //将这些任务数据添加到消费者队列中
-                cacheService.refreshWithPipeline(futureKey, topicKey, tasks);
-                System.out.println("成功的将" + futureKey + "下的当前需要执行的任务数据刷新到" + topicKey + "下");
+        if (StringUtils.isNotBlank(token)){
+            log.info("未来数据定时刷新---定时任务");
+
+            // 获取所有未来数据集合的key值
+            Set<String> futureKeys = cacheService.scan(ScheduleConstants.FUTURE + "*");// future_*
+            for (String futureKey : futureKeys) { // future_250_250
+
+                // 获取当前数据的  key  topic
+                String topicKey = ScheduleConstants.TOPIC + futureKey.split(ScheduleConstants.FUTURE)[1];
+                //获取该组key下当前需要消费的任务数据
+                Set<String> tasks = cacheService.zRangeByScore(futureKey, 0, System.currentTimeMillis());
+                if (!tasks.isEmpty()) {
+                    //将这些任务数据添加到消费者队列中
+                    cacheService.refreshWithPipeline (futureKey, topicKey, tasks);
+                    log.info("成功的将"+futureKey+"刷新到了"+topicKey);
+                }
             }
         }
     }
